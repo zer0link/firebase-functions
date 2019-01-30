60e00c0159a5f3dd7aee47f56541d3488256800d
Chong Han Khee <key@Chongs-MacBook-Pro.local>
2019-01-30 21:20:11 +0800

    push notification 001

--- functions/index.js
+++ functions/index.js
@@ -16,10 +16,37 @@ exports.doThings = functions.database.ref('DeviceLocation/{id}').onWrite((change
     return "success";
   });
 
-exports.sendGCMNotification = functions.database.ref('DeviceLocation').onWrite((change,context) =>{
+  exports.sendGCMNotification = functions.database.ref('DeviceLocation').onWrite((change,context) =>{
+    var registrationToken = "<registration token here>";
+
+    var topic = "finance";
+
+    admin.messaging().subscribeToTopic('eITSjt15ZFk:APA91bGXw2XvbCjgQ7Nx-1hvzJTcKg1uyKgG6pf2Syc9EmnQgX_F9vKhizo0UtiluMKn5nWM42LtNdtI8VJLSZbaFEjm2Py1z0M5-9Uf3y97RxIpJ_n3XFWO-8HYS54Ab_kwmGvZN8-G', topic)
+      .then(function(response) {
+        console.log("Successfully subscribed to topic:", response);
+      })
+      .catch(function(error) {
+        console.log("Error subscribing to topic:", error);
+      });
+  
+    
+    const payload = {
+      notification: {
+        title: 'You have a new follower!',
+        body: `I am now following you.`
+      }
+    };
+  
+    // Listing all tokens as an array.
+    tokens = ['eITSjt15ZFk:APA91bGXw2XvbCjgQ7Nx-1hvzJTcKg1uyKgG6pf2Syc9EmnQgX_F9vKhizo0UtiluMKn5nWM42LtNdtI8VJLSZbaFEjm2Py1z0M5-9Uf3y97RxIpJ_n3XFWO-8HYS54Ab_kwmGvZN8-G'];
+  
+    return admin.messaging().sendToTopic('finance', payload);
+  });
+
+exports.sendGCMNotification2 = functions.database.ref('DeviceLocation').onWrite((change,context) =>{
   const payload = {
     notification: {
-      title: 'You have a new follower!',
+      title: 'You have a new follower2!',
       body: `I am now following you.`
     }
   };
